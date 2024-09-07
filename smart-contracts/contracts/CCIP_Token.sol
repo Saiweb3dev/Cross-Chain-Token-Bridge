212309014a1c7e340715a354fca0e7c4bb61f3e2
saik434455@gmail.com <saik434455@gmail.com>
2024-09-07 11:52:30 +0530

    feat : code refactored and also removed some unused functions

--- smart-contracts/contracts/CCIP_Token.sol
+++ smart-contracts/contracts/CCIP_Token.sol
@@ -5,53 +5,50 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 
-// Custom error definitions
-error InvalidAmount(uint256 amount);
-error InsufficientBalance(address account, uint256 balance, uint256 required);
-error UnauthorizedAccess(address caller);
-error ZeroAddress();
-error InsufficientSupply(uint256 available, uint256 requested);
-
 /**
  * @title CCIPToken
  * @dev Implementation of the CCIPToken
  * This contract extends ERC20, Ownable, and Pausable functionalities.
  */
 contract CCIPToken is ERC20, Ownable, Pausable {
-    uint256 private _initialSupply;
+    // Custom error declarations
+    error InvalidAmount(uint256 amount);
+    error InsufficientBalance(address account, uint256 balance, uint256 required);
+    error UnauthorizedAccess(address caller);
+    error ZeroAddress();
+    error InsufficientSupply(uint256 available, uint256 requested);
+
+    // State variables
+    uint256 private _tokenSupply;
     uint256 private _availableSupply;
-    uint256 private _tokenInSupply;
     address private vaultAddress;
 
-    // Mapping to track transfer amounts between addresses
-    mapping(address => mapping(address => uint256)) public transferAmounts;
-    // Mapping to track transfer addresses for each address
-    mapping(address => address[]) public transferAddresses;
-    // Mapping to track locked amounts for each address
-    mapping(address => uint256) public lockedAmounts;
+    // Named Mappings
+    mapping(address sender => mapping(address recipient => uint256 amount)) public transferAmounts;
+    mapping(address sender => address[] recipients) public transferAddresses;
+    mapping(address user => uint256 amount) public lockedAmounts;
     
     // Events
     event Mint(address indexed to, uint256 amount);
     event Burn(address indexed from, uint256 amount);
     event CurrentOwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event TokenSupplyIncreased(uint256 amount);
 
     /**
-     * @dev Constructor that gives msg.sender all of existing tokens.
-     * @param name The name of the token
-     * @param symbol The symbol of the token
-     * @param __initialSupply The initial supply of tokens
+     * @dev Constructor that sets up the CCIPToken.
+     * @param name The name of the token.
+     * @param symbol The symbol of the token.
+     * @param __tokenSupply The initial token supply.
      */
-    constructor(string memory name, string memory symbol, uint256 __initialSupply)
+    constructor(string memory name, string memory symbol, uint256 __tokenSupply)
         ERC20(name, symbol)
+        Ownable(msg.sender)
     {
-        _initialSupply = __initialSupply;
-        _availableSupply = __initialSupply;
-        _mint(msg.sender, __initialSupply);
+        _tokenSupply = __tokenSupply;
+        _availableSupply = __tokenSupply;
     }
 
-    /**
-     * @dev Modifier to validate if the amount is greater than zero
-     */
+    // Modifiers
     modifier validAmount(uint256 amount) {
         if (amount == 0) {
             revert InvalidAmount(amount);
@@ -59,18 +56,17 @@ contract CCIPToken is ERC20, Ownable, Pausable {
         _;
     }
 
-    /**
-     * @dev Modifier to restrict access to only the vault address
-     */
     modifier onlyVault() {
         require(msg.sender == vaultAddress, "Caller is not the vault");
         _;
     }
 
+    // External and public functions (non-view)
+
     /**
-     * @dev Function to mint tokens
-     * @param to The address that will receive the minted tokens
-     * @param amount The amount of tokens to mint
+     * @dev Mints new tokens.
+     * @param to The address that will receive the minted tokens.
+     * @param amount The amount of tokens to mint.
      */
     function mint(address to, uint256 amount)
         public
@@ -84,14 +80,13 @@ contract CCIPToken is ERC20, Ownable, Pausable {
         }
         _mint(to, amount);
         _availableSupply -= amount;
-        _tokenInSupply += amount;
         emit Mint(to, amount);
     }
 
     /**
-     * @dev Function to burn tokens
-     * @param from The address from which to burn tokens
-     * @param amount The amount of tokens to burn
+     * @dev Burns tokens.
+     * @param from The address from which to burn tokens.
+     * @param amount The amount of tokens to burn.
      */
     function burn(address from, uint256 amount)
         public
@@ -104,43 +99,25 @@ contract CCIPToken is ERC20, Ownable, Pausable {
             revert InsufficientBalance(from, balanceOf(from), amount);
         }
         _burn(from, amount);
-        _tokenInSupply -= amount;
+        _availableSupply += amount;
         emit Burn(from, amount);
     }
 
     /**
-     * @dev Returns the initial supply of tokens
+     * @dev Increases the token supply.
+     * @param amount The amount to increase the supply by.
      */
-    function initialSupply() public view returns (uint256) {
-        return _initialSupply;
+    function increaseTokenSupply(uint256 amount) public onlyOwner {
+        _tokenSupply += amount;
+        _availableSupply += amount;
+        emit TokenSupplyIncreased(amount);
     }
 
     /**
-     * @dev Returns the available supply of tokens
-     */
-    function availableSupply() public view returns (uint256) {
-        return _availableSupply;
-    }
-
-    /**
-     * @dev Returns the current supply of tokens in circulation
-     */
-    function tokenInSupply() public view returns (uint256) {
-        return _tokenInSupply;
-    }
-
-    /**
-     * @dev Returns the balance of the specified account
-     * @param account The address to query the balance of
-     */
-    function balanceOf(address account) public view override returns (uint256) {
-        return super.balanceOf(account);
-    }
-
-    /**
-     * @dev Transfer token to a specified address
-     * @param recipient The address to transfer to
-     * @param amount The amount to be transferred
+     * @dev Transfers tokens to a specified address.
+     * @param recipient The address to transfer to.
+     * @param amount The amount to be transferred.
+     * @return A boolean that indicates if the operation was successful.
      */
     function transfer(address recipient, uint256 amount)
         public
@@ -153,8 +130,8 @@ contract CCIPToken is ERC20, Ownable, Pausable {
     }
 
     /**
-     * @dev Transfer tokens to the CCIP vault
-     * @param _amount The amount of tokens to transfer
+     * @dev Transfers tokens to the CCIP vault.
+     * @param _amount The amount to transfer.
      */
     function transferToCCIPVault(uint256 _amount) public {
         _transfer(msg.sender, vaultAddress, _amount);
@@ -162,17 +139,9 @@ contract CCIPToken is ERC20, Ownable, Pausable {
     }
 
     /**
-     * @dev Get the locked amount for a specific user
-     * @param _user The address of the user
-     */
-    function getLockAmount(address _user) public view returns(uint256) {
-        return lockedAmounts[_user];
-    }
-
-    /**
-     * @dev Burn locked amount for a specific user
-     * @param _user The address of the user
-     * @param _amount The amount to burn
+     * @dev Burns the locked amount for a user.
+     * @param _user The address of the user.
+     * @param _amount The amount to burn.
      */
     function burnLockAmount(address _user, uint256 _amount) public onlyVault {
         require(lockedAmounts[_user] >= _amount, "Insufficient locked amount");
@@ -180,8 +149,8 @@ contract CCIPToken is ERC20, Ownable, Pausable {
     }
 
     /**
-     * @dev Set the vault address
-     * @param _newVaultAddress The new vault address
+     * @dev Sets the vault address.
+     * @param _newVaultAddress The new vault address.
      */
     function setVaultAddress(address _newVaultAddress) public onlyOwner {
         require(_newVaultAddress != address(0), "Invalid address");
@@ -189,10 +158,11 @@ contract CCIPToken is ERC20, Ownable, Pausable {
     }
 
     /**
-     * @dev Transfer tokens from one address to another
-     * @param sender The address which you want to send tokens from
-     * @param recipient The address which you want to transfer to
-     * @param amount The amount of tokens to be transferred
+     * @dev Transfers tokens from one address to another.
+     * @param sender The address to transfer from.
+     * @param recipient The address to transfer to.
+     * @param amount The amount to be transferred.
+     * @return A boolean that indicates if the operation was successful.
      */
     function transferFrom(
         address sender,
@@ -204,69 +174,8 @@ contract CCIPToken is ERC20, Ownable, Pausable {
     }
 
     /**
-     * @dev Count the number of transfers between addresses
-     * @param _from The sender's address
-     * @param _to The recipient's address
-     * @param _amount The amount transferred
-     */
-    function countNumber(
-        address _from,
-        address _to,
-        uint256 _amount
-    ) external {
-        transferAmounts[_from][_to] += _amount;
-        if (transferAmounts[_from][_to] > 0 && !containsAddress(_from, _to)) {
-            transferAddresses[_from].push(_to);
-        }
-    }
-
-    /**
-     * @dev Check if an address is contained in the transfer addresses of another address
-     * @param _address The main address
-     * @param _otherAddress The address to check for
-     */
-    function containsAddress(address _address, address _otherAddress)
-        internal
-        view
-        returns (bool)
-    {
-        address[] memory addresses = transferAddresses[_address];
-        for (uint256 i = 0; i < addresses.length; i++) {
-            if (addresses[i] == _otherAddress) return true;
-        }
-        return false;
-    }
-
-    /**
-     * @dev Get the total transfer count from an address
-     * @param _address The address to check
-     */
-    function getTransferCountFromAddress(address _address)
-        public
-        view
-        returns (uint256)
-    {
-        uint256 totalCount = 0;
-        address[] memory toAddresses = transferAddresses[_address];
-
-        for (uint256 i = 0; i < toAddresses.length; i++) {
-            totalCount += transferAmounts[_address][toAddresses[i]];
-        }
-
-        return totalCount;
-    }
-
-    /**
-     * @dev Returns the address of the current owner
-     */
-    function OwnerAddress() public view returns (address) {
-        return owner();
-    }
-
-    /**
      * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Can only be called by the current owner.
-     * @param newOwner The address of the new owner
+     * @param newOwner The address of the new owner.
      */
     function transferOwnership(address newOwner) public virtual override onlyOwner {
         if (newOwner == address(0)) {
@@ -274,5 +183,49 @@ contract CCIPToken is ERC20, Ownable, Pausable {
         }
         _transferOwnership(newOwner);
         emit CurrentOwnershipTransferred(owner(), newOwner);
+    }
+
+    // Public view functions
+
+    /**
+     * @dev Returns the initial supply of tokens.
+     * @return The initial token supply.
+     */
+    function initialSupply() public view returns (uint256) {
+        return _tokenSupply;
+    }
+
+    /**
+     * @dev Returns the available supply of tokens.
+     * @return The available token supply.
+     */
+    function availableSupply() public view returns (uint256) {
+        return _availableSupply;
+    }
+
+    /**
+     * @dev Returns the total supply of tokens.
+     * @return The total token supply.
+     */
+    function totalSupply() public view override returns (uint256) {
+        return _tokenSupply - _availableSupply;
+    }
+
+    /**
+     * @dev Returns the balance of the given account.
+     * @param account The address to query the balance of.
+     * @return The balance of the given account.
+     */
+    function balanceOf(address account) public view override returns (uint256) {
+        return super.balanceOf(account);
+    }
+
+    /**
+     * @dev Returns the locked amount for a user.
+     * @param _user The address of the user.
+     * @return The locked amount.
+     */
+    function getLockAmount(address _user) public view returns(uint256) {
+        return lockedAmounts[_user];
     }
 }
